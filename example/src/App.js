e7a3b3da67684eb7803f2f1d3adef417d10ced7a
zoi10010 <denis.voice@zoifintech.com>
2021-10-19 19:14:26 +0530

    button Modules

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-import { ZButton } from 'zoinodemodules'
-import 'zoinodemodules/dist/index.css'
+import { ZButton,ZButtonProgress } from 'zoinodemodules'
+import '../src/index.css'
+import AwesomeButtonStyles from "zoinodemodules/src/styles/index";
 
 const App = () => {
-  return <ZButton text="Create React Library Example 😄" />
+  return <div style={{margin:100}}><ZButton style={{width : 100}}
+  size="small"
+  type="primary"
+>
+  Large
+</ZButton>
+<ZButtonProgress
+  type="secondary"
+  size="medium"
+  action={(element, next) => console.log(next)}
+>
+  Primary
+</ZButtonProgress>
+</div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ZButton = ({ text, style, className, onClick }) => {
-  return <input className={className ? className : styles.test} style={style} type="button" onClick={onClick} value={text ? text : "Submit"} />
-}
-
-export const ZTextBox = ({ value, style, className,onChange }) => {
-  return <input className={className ? className : styles.test}  onChange={onChange} style={style} type="text" value={value} />
-}
+export { default as ZButton } from './components/AwesomeButton';
+export { default as ZButtonSocial } from './components/AwesomeButtonSocial';
+export { default as ZButtonProgress } from './components/AwesomeButtonProgress';
